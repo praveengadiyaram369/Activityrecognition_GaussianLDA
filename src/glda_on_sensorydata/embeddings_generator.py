bcb623868534ed90ba24b1521f8c654509099cb0
praveengadiyaram369 <praveengadiyaram@gmail.com>
2020-12-22 19:58:14 +0100

    adding changes for normalization testing

--- src/glda_on_sensorydata/embeddings_generator.py
+++ src/glda_on_sensorydata/embeddings_generator.py
@@ -10,6 +10,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
+cluster_vocab = ['X1_A', 'X1_B', 'X1_C', 'Y1_A', 'Y1_B', 'Y1_C', 'Y1_D', 'Z1_A', 'Z1_B',
+                 'Z1_C', 'X2_A', 'X2_B', 'X2_C', 'Y2_A', 'Y2_B', 'Y2_C', 'Z2_A', 'Z2_B', 'Z2_C']
+remove_words = ['Z2_A', 'Y2_A', 'X2_A']
+remove_ind = [cluster_vocab.index(val) for val in remove_words]
+
 
 def load_data(input_txt_filepath):
 
@@ -18,7 +23,7 @@ def load_data(input_txt_filepath):
 
     sample_list = []
     activity_list = []
-    remove_words = ['Z2_A', 'Y2_A', 'X2_B']
+
     for txt_file in txt_files:
         tmp_list = []
         activity = txt_file.split('/')[-1].split('_')[-1].split('.')[0]
@@ -27,15 +32,14 @@ def load_data(input_txt_filepath):
         data = (open(txt_file, "r")).read().splitlines()[:1350]
         for doc in data:
             d = doc.split(' ')
-            #removing most frequent words
+            # removing most frequent words
             for i in d:
                 if i not in remove_words:
                     tmp_list.append(i)
-            
+
         sample_list.append(tmp_list)
-        #count_data = collections.Counter(tmp_list)
-        #print('*****', count_data)
-        
+        count_data = collections.Counter(tmp_list)
+        print('*****', count_data)
 
     return sample_list, activity_list
 
@@ -67,20 +71,19 @@ def get_cluster_embeddings(input_txt_filepath, embeddings_filepath):
 
     samples, activities = load_data(input_txt_filepath)
     embeddings = []
-    remove_ind = [11, 13, 16]
+
+    vocab = cluster_vocab.copy()
     data = (open(embeddings_filepath, "r")).read().splitlines()
-    
-    #removing most frequent word embeddings
+
+    # removing most frequent word embeddings
     for index in sorted(remove_ind, reverse=True):
         del data[index]
-    
+        del vocab[index]
+
     for emb in data:
         embeddings.append(emb.split(','))
 
     cluster_embeddings = np.asarray(embeddings, dtype=float)
-
-    vocab = ['X1_A', 'X1_B', 'X1_C', 'Y1_A', 'Y1_B', 'Y1_C', 'Y1_D', 'Z1_A', 'Z1_B',
-             'Z1_C', 'X2_A', 'X2_C', 'Y2_B', 'Y2_C', 'Z2_B', 'Z2_C']
 
     corpus = get_corpus(vocab, samples)
 
@@ -143,15 +146,19 @@ if __name__ == "__main__":
     # _manual testing purpose
     input_txt_filepath = os.getcwd() + f'/../../data/sub_sequence_output/*activity*.txt'
     samples, activities = load_data(input_txt_filepath)
-    #print('#####',activities)
+    # print('#####',activities)
 
-    model = get_model(samples)
-    print(model)
+    # model = get_model(samples)
+    # print(model)
 
     #vocab = list(model.wv.vocab.keys())
     # print(vocab)
     # print(model.wv.vectors)
 
-    #plot_pca(model.wv.vocab)
-    #plt.rcParams["figure.figsize"] = [16, 9]
+    # plot_pca(model.wv.vocab)
+    plt.rcParams["figure.figsize"] = [16, 9]
     #plot_vector_similarity(vocab, model.wv.vectors)
+    # data = (open(os.getcwd(
+    # ) + f'/../../data/sub_sequence_output/word_embeddings_from_clusters.txt', "r")).read().splitlines()
+    # embeddings = [emb.split(',') for emb in data]
+    # plot_vector_similarity(cluster_vocab, embeddings)
