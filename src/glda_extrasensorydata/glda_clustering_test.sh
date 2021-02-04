ab7b2dacac394c7092bae97fbb0d45f7e0a4a1ee
praveengadiyaram369 <praveengadiyaram@gmail.com>
2021-02-04 20:30:07 +0100

    adding changes for glda testing

--- src/glda_extrasensorydata/glda_clustering_test.sh
+++ src/glda_extrasensorydata/glda_clustering_test.sh
@@ -1,2 +1,22 @@
 python clustering_features.py kmeans 10
 python clustering_features.py gmm 20
+
+# python clustering_features.py kmeans 100
+# python clustering_features.py kmeans 500
+# python clustering_features.py kmeans 1000
+# python clustering_features.py kmeans 3000
+# python clustering_features.py kmeans 5000
+# python clustering_features.py kmeans 7000
+# python clustering_features.py kmeans 10000
+# python clustering_features.py kmeans 12000
+# python clustering_features.py kmeans 15000
+
+# python clustering_features.py gmm 100
+# python clustering_features.py gmm 500
+# python clustering_features.py gmm 1000
+# python clustering_features.py gmm 3000
+# python clustering_features.py gmm 5000
+# python clustering_features.py gmm 7000
+# python clustering_features.py gmm 10000
+# python clustering_features.py gmm 12000
+# python clustering_features.py gmm 15000
