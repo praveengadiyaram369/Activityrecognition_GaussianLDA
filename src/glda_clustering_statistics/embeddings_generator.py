3ba99e637fb05ae96db39da0be2c340a61c7f441
NagaKartheekReddy <kartheek.5081@gmail.com>
2021-02-18 20:42:46 +0100

    code changes to generate more train and test documents

--- src/glda_clustering_statistics/embeddings_generator.py
+++ src/glda_clustering_statistics/embeddings_generator.py
@@ -34,40 +34,39 @@ def load_data(input_txt_filepath):
 
     for txt_file in txt_files:
 
+        start_ind = 0
         tmp_list = []
         activity = txt_file.split('/')[-1].split('_')[-1].split('.')[0]
         label = 'activity_'+str(activity)
 
         data = (open(txt_file, "r")).read().splitlines()
 
-        for doc in data:
-            tmp_list.extend(doc.split(' '))
+        while start_ind < len(data):
 
-        # train_doc, test_doc = train_test_split(
-        #     tmp_list, test_size=0.25, random_state=1)
+            end_ind = start_ind + 10
+            for doc in data[start_ind:end_ind]:
+                tmp_list.append(doc.split(' '))
+            start_ind = end_ind
 
-        # train_docs.append(train_doc)
-        # test_docs.append(test_doc)
+        train_doc, test_doc = train_test_split(
+             tmp_list, test_size=0.10, random_state=1)
+
+        train_docs.extend(train_doc)
+        test_docs.extend(test_doc)
+        list_indexes, doc_count = get_indexes_train_docs(train_doc, doc_count)
 
         # # train_doc_labels.append(label)
-        # test_doc_labels.append(label)
+        test_doc_labels.extend([label] * len(test_doc))
 
-        doc_count += 1
         if label not in activity_doc_count_index:
-            activity_doc_count_index[label] = [doc_count]
+            activity_doc_count_index[label] = list_indexes
         else:
-            if len(activity_doc_count_index[label]) > 5:
-                doc_count -= 1
-                test_docs.append(tmp_list)
-                test_doc_labels.append(label)
-                continue
-
-            activity_doc_count_index[label].append(doc_count)
+            activity_doc_count_index[label].extend(list_indexes)
 
         train_docs.append(tmp_list)
         activity_list.append('activity_'+str(activity))
 
-    # print(activity_doc_count_index)
+    
     return train_docs, activity_list, activity_doc_count_index
 
 
@@ -85,7 +84,7 @@ def filter_embeddings(vocab, embeddings):
     cluster_names = generate_cluster_names(sequence_names, cluster_cnt)
 
     total_clusters = []
-    for key, val in cluster_names.items():
+    for val in cluster_names:
         total_clusters.extend(val)
 
     final_vocab = []
@@ -128,3 +127,15 @@ def get_cluster_embeddings(input_txt_filepath, embeddings_filepath):
 
 def get_test_documents():
     return test_docs, test_doc_labels
+
+
+def get_indexes_train_docs(train_docs, doc_count):
+
+    doc_index_list = []
+
+    for _ in range(len(train_docs)):
+
+        doc_count += 1
+        doc_index_list.append(doc_count)
+    
+    return doc_index_list, doc_count
