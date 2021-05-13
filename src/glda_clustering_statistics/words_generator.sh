d55247a5da22e82eb1ecddbac2f45ef715f783d8
NagaKartheekReddy <kartheek.5081@gmail.com>
2021-05-13 13:57:51 +0200

    Best 3 cases

--- src/glda_clustering_statistics/words_generator.sh
+++ src/glda_clustering_statistics/words_generator.sh
@@ -1,35 +1,8 @@
-python histogram_features.py 6 2 10 64 64
-python glda_clustering.py 10 64 64
-
-python histogram_features.py 6 2 10 64 48
-python glda_clustering.py 10 64 48
-
-python histogram_features.py 6 2 10 64 32
-python glda_clustering.py 10 64 32
-
-python histogram_features.py 6 2 10 32 32
-python glda_clustering.py 10 32 32
-
-python histogram_features.py 6 2 10 32 24
-python glda_clustering.py 10 32 24
-
 python histogram_features.py 6 2 10 32 16
 python glda_clustering.py 10 32 16
 
 python histogram_features.py 6 2 10 16 16
 python glda_clustering.py 10 16 16
 
-python histogram_features.py 6 2 10 16 12
-python glda_clustering.py 10 16 12
-
 python histogram_features.py 6 2 10 16 8
 python glda_clustering.py 10 16 8
-
-python histogram_features.py 6 2 10 8 8
-python glda_clustering.py 10 8 8
-
-python histogram_features.py 6 2 10 8 6
-python glda_clustering.py 10 8 6
-
-python histogram_features.py 6 2 10 8 4
-python glda_clustering.py 10 8 4
