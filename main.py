0841bedcaa0de8cdbd805ad30a1c3596c0de7b3d
praveengadiyaram369 <praveengadiyaram@gmail.com>
2021-08-07 00:21:46 +0200

    adding changes for UI

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from starlette.responses import FileResponse
-from starlette.routing import Host 
 import uvicorn
 
 app = FastAPI(debug=True)
@@ -14,16 +12,16 @@ async def read_index(request: Request):
     return templates.TemplateResponse('index.html', context={'request': request, 'options_list': test_samples_idx, 'instance_data':default_data})
 
 @app.post('/get_test_instance')
-async def load_instance(request: Request, doc_id: int=0):
+async def load_instance(request: Request, doc_id: int=Form(...)):
     print(doc_id)
-    output = {'id':doc_id, 'X1': [doc_id, doc_id], 'Y1': [1,2], 'Z1': [1,2], 'X2': [1,2], 'Y2': [1,2], 'Z2': [1,2], }
+    output = {'id':doc_id, 'X1': [doc_id, doc_id], 'Y1': [1,2], 'Z1': [1,2], 'X2': [1,2], 'Y2': [1,2], 'Z2': [1,2] }
     return output
 
 @app.post('/predict_instance')
 async def load_instance(request: Request, doc_id: int=Form(...)):
     print(doc_id)
-    output = {'id':doc_id, 'X1': [doc_id, doc_id], 'Y1': [1,2], 'Z1': [1,2], 'X2': [1,2], 'Y2': [1,2], 'Z2': [1,2], }
-    return templates.TemplateResponse('index.html', context={'request': request, 'options_list': test_samples_idx, 'instance_data':output, 'output': output})
+    output = {'id':doc_id, 'X1': [doc_id, doc_id], 'Y1': [1,2], 'Z1': [1,2], 'X2': [1,2], 'Y2': [1,2], 'Z2': [1,2] }
+    return templates.TemplateResponse('index.html', context={'request': request, 'options_list': test_samples_idx, 'instance_data':default_data, 'prev_instance_data':output,'output': output})
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port='8080')
